d966052fd5507cdfa655d6c93344f42df8422f1d
trevorGit <your.email@westernsydney.edu.au>
2026-08-06 16:49:09 +1000

    improvement idea

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const frenchHelloWorld = 'Bonjout Le Monde'
 const germanHelloWorld = 'Hallo, Welt!'
 const italianHelloWorld = 'Ciao Mondo'
 console.log(italianHelloWorld);
+
+//Improvement Demo
+function greet(message){
+    console.log(message)
+}
+greet('Hello World');
