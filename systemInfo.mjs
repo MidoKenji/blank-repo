b9932953ce2628010823221c6dcfc9b7bf88b6b4
trevorGit <your.email@westernsydney.edu.au>
2026-08-13 04:15:56 +0000

    deleting left over last week file

---
+++ systemInfo.mjs
@@ -0,0 +1,10 @@
+//Referrence DOC: https://nodejs.org/api/os.html
+
+/**
+ * Relevant Stuff:
+ * os.platform() - returns well the fucking platform
+ * os.type() - i think this is just kernel
+ * os.release() - os release/kernel version number
+ * os.freemem()/ os.totalmem() both returns in bytes (which is what the )
+ * 
+ */
